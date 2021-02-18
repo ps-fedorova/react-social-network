66051da4ac4047af1307df113e5cc774b7b5a260
Polina Fedorova <ps.fedorova@yandex.ru>
2021-02-18 23:18:03 +0500

    refactor: add class components

--- src/components/Main/Users/FindUsers.js
+++ src/components/Main/Users/FindUsers.js
@@ -3,32 +3,75 @@ import s from "../../../utils/ui-kit/UserCard/UserCard.module.css"
 import UserCard from "../../../utils/ui-kit/UserCard/UserCard";
 import * as axios from "axios";
 
-const FindUsers = ({users, followed, unfollowed, setUsers}) => {
-
-  if (users.length === 0) {
-
-    axios.get("https://social-network.samuraijs.com/api/1.0/users")
-      .then(res => {
-        setUsers(res.data.items);
-        console.log("юзеры пришли");
-      })
-      .catch(() => console.log("юзеры не пришли"));
+class FindUsers extends React.Component {
+  constructor(props) {
+    super(props);
+    // if (this.props.users.length === 0) { // конструктор вызывается итак и раз
+      axios.get("https://social-network.samuraijs.com/api/1.0/users")
+        .then(res => {
+          this.props.setUsers(res.data.items);
+          console.log("юзеры пришли");
+        })
+        .catch(() => console.log("юзеры не пришли"));
+      // setUsers([
+      //   {
+      //     id: 0,
+      //     name: "Дмитрий",
+      //     followed: true,
+      //     location: {
+      //       city: "Город",
+      //       country: "Страна",
+      //     }
+      //   },
+      //   {
+      //     id: 1,
+      //     name: "Дмитрий",
+      //     followed: true,
+      //     location: {
+      //       city: "Город",
+      //       country: "Страна",
+      //     }
+      //   },
+      //   {
+      //     id: 2,
+      //     name: "Дмитрий",
+      //     followed: false,
+      //     location: {
+      //       city: "Город",
+      //       country: "Страна",
+      //     }
+      //   },
+      //   {
+      //     id: 3,
+      //     name: "Дмитрий",
+      //     followed: false,
+      //     location: {
+      //       city: "Город",
+      //       country: "Страна",
+      //     }
+      //   },
+      // ])
+    // }
   }
 
-  return (
-    <div className={s.findUsers}>
-      <ul className="list">
-        {users.map((user, index) =>
-          <UserCard
-            user={user}
-            key={index}
-            followed={followed}
-            unfollowed={unfollowed}
-            setUsers={setUsers}
-          />)}
-      </ul>
-    </div>
-  )
+  render() {
+    const { users, followed, unfollowed, setUsers } = this.props
+    return(
+
+      <div className={s.findUsers}>
+        <ul className="list">
+          {users.map((user, index) =>
+            <UserCard
+              user={user}
+              key={index}
+              followed={followed}
+              unfollowed={unfollowed}
+              setUsers={setUsers}
+            />)}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default FindUsers;
--- src/components/Main/Users/Friends.js
+++ src/components/Main/Users/Friends.js
@@ -3,69 +3,78 @@ import UserCard from "../../../utils/ui-kit/UserCard/UserCard";
 import s from "../../../utils/ui-kit/UserCard/UserCard.module.css"
 import * as axios from "axios";
 
-const Friends = ({ users, followed, unfollowed, setUsers }) => {
+class Friends extends React.Component {
 
-  if (users.length === 0) {
-    axios.get("https://social-network.samuraijs.com/api/1.0/users")
-      .then(res => {
-        setUsers(res.data.items);
-        console.log("юзеры пришли");
-      })
-      .catch(() => console.log("юзеры не пришли"));
-    // setUsers([
-    //   {
-    //     id: 0,
-    //     name: "Дмитрий",
-    //     followed: true,
-    //     location: {
-    //       city: "Город",
-    //       country: "Страна",
-    //     }
-    //   },
-    //   {
-    //     id: 1,
-    //     name: "Дмитрий",
-    //     followed: true,
-    //     location: {
-    //       city: "Город",
-    //       country: "Страна",
-    //     }
-    //   },
-    //   {
-    //     id: 2,
-    //     name: "Дмитрий",
-    //     followed: false,
-    //     location: {
-    //       city: "Город",
-    //       country: "Страна",
-    //     }
-    //   },
-    //   {
-    //     id: 3,
-    //     name: "Дмитрий",
-    //     followed: false,
-    //     location: {
-    //       city: "Город",
-    //       country: "Страна",
-    //     }
-    //   },
-    // ])
+  addUsers = () => {
+    if (this.props.users.length === 0) {
+      axios.get("https://social-network.samuraijs.com/api/1.0/users")
+        .then(res => {
+          this.props.setUsers(res.data.items);
+          console.log("юзеры пришли");
+        })
+        .catch(() => console.log("юзеры не пришли"));
+      // setUsers([
+      //   {
+      //     id: 0,
+      //     name: "Дмитрий",
+      //     followed: true,
+      //     location: {
+      //       city: "Город",
+      //       country: "Страна",
+      //     }
+      //   },
+      //   {
+      //     id: 1,
+      //     name: "Дмитрий",
+      //     followed: true,
+      //     location: {
+      //       city: "Город",
+      //       country: "Страна",
+      //     }
+      //   },
+      //   {
+      //     id: 2,
+      //     name: "Дмитрий",
+      //     followed: false,
+      //     location: {
+      //       city: "Город",
+      //       country: "Страна",
+      //     }
+      //   },
+      //   {
+      //     id: 3,
+      //     name: "Дмитрий",
+      //     followed: false,
+      //     location: {
+      //       city: "Город",
+      //       country: "Страна",
+      //     }
+      //   },
+      // ])
+    }
   }
 
-  return (
-    <div className={s.findUsers}>
-      <ul className="list">
-        {users.filter(user => user.followed).map((user) =>
-          <UserCard
-            user={user}
-            key={user.id}
-            followed={followed}
-            unfollowed={unfollowed}
-            setUsers={setUsers}
-          />
-        )}
-      </ul>
-    </div>
-  )
+ render() {
+
+   const { users, followed, unfollowed, setUsers } = this.props
+
+   return (
+     <div className={s.findUsers} >
+       <button onClick={this.addUsers}/>
+       <ul className="list">
+         {users.filter(user => user.followed).map((user) =>
+           <UserCard
+             user={user}
+             key={user.id}
+             followed={followed}
+             unfollowed={unfollowed}
+             setUsers={setUsers}
+           />
+         )}
+       </ul>
+     </div>
+   )
+ }
 }
+
 export default Friends;
