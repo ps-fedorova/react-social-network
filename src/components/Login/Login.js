37be1288607e867150cf5c55e21d6fa2a2c126a3
Polina Fedorova <ps.fedorova@yandex.ru>
2021-03-03 23:19:13 +0500

    feat: add a login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,32 @@
-import React from "react"
+import React from "react";
+import s from "./Login.module.css"
+
+// Create New Account
 
 const Login = () => {
-  return <h1>LOGIN</h1>
+  return (
+    <form className={s.form}>
+      <div className={s.position}>
+        <h2 className={s.h2}>Login</h2>
+
+        <label className={s.label} htmlFor="email">
+          <input className={s.input} type="text" placeholder="email" id="email" required/>
+        </label>
+
+        <label className={s.label} htmlFor="psw">
+          <input className={s.input} type="password" placeholder="password" id="password" required/>
+        </label>
+
+        <div className={s["rememberMe-container"]}>
+          <label className={s["rememberMe-label"]} htmlFor="rememberMe">remember me</label>
+          <input className={s.rememberMe} type="checkbox" id="rememberMe"/>
+        </div>
+
+        <button className={`button ${s.button}`} type="submit">Login</button>
+      </div>
+    </form>
+  )
+
 }
 
 export default Login;
